5f17a340cbda66761b3050989d512dc71f566ded
todd-cook <665389+todd-cook@users.noreply.github.com>
2023-04-27 00:48:57 -0700

    clean up

--- runUnitTests.sh
+++ runUnitTests.sh
@@ -2,7 +2,6 @@
 # runUnitTests.sh
 # export COVERAGE_DEBUG=process,config
 black src
-# python -m pytest --doctest-modules --mypy --pylint -vv src/
 python -m pytest --doctest-modules --cov-report=term --mypy --cov-config=.coveragerc \
 --pylint -vv src/  --cov=src
 find . -name '.coverage' -type f -delete
